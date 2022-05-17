ac6e7dced40a66a867043ecd24a6e7b5952187f9
alan-w25 <alan.wusig@gmail.com>
2022-05-17 16:37:29 -0400

    added js for functional app

--- main.js
+++ main.js
@@ -1,56 +1,77 @@
+
+//selectors 
+const choices = document.querySelectorAll("button");
+
+let currentPlayerScore = 0; 
+let currentComputerScore = 0; 
+console.log(currentPlayerScore)
+
+function disable() {
+    choices.forEach(elem => {
+        elem.disabled = true
+    })
+}
 function computerPlay(){
     let randInt = Math.floor(Math.random()*3);
-    if(randInt===0){
-        return "Rock"
-    } else if (randInt===1){
-        return "Paper"
-    } else if(randInt===2) {
-        return "Scissors"
-    }
-}
-function playRound(playerSelection,computerSelection){
-    let player = playerSelection.toLowerCase()
-    let computer = computerSelection.toLowerCase()
-    let winner = "Computer"
-    let message = "Tie!"
-    if(player === computer){
-        console.log(message)
+    if(randInt == 0){
+        return "rock";
+    } else if(randInt == 1){
+        return "paper";
+    } else if(randInt == 2){
+        return "scissors";
     } 
-    if(player === "rock"){
-        if(computer === "paper"){
-            winner = "Computer"
-            message = "Paper beats Rock!"
-        } else if (computer === "scissors"){
-            winner = "Player"
-            message = "Rock beats Scissors!"
-        }
-    } else if (player==="paper"){
-        if(computer === "scissors"){
-            winner=="Computer"
-            message="Scissors beats Paper!"
-        } else if (computer==="rock"){
-            winner="Player"
-            message = "Paper beats Rock!"
-        }
-    } else if (player === "scissors"){
-        if(computer==="rock"){
-            winner="Computer"
-            message = "Rock beats Scissors!"
-        } else if(computer==="paper"){
-            winner="Player"
-            message="Scissors beats Paper!"
-        }
-    }
-    return winner + " wins! " + message
-
-}
-function game(){
-    
-    for(let i = 0; i < 5; i++){
-        let playerSelect = prompt("Enter Rock Paper or Scissors")
-        playRound(playerSelect,computerPlay())
-    }
 }
 
+function playRound(playerSelection){
+    let player = playerSelection.toLowerCase();
+    let computer = computerPlay().toLowerCase();
+    let winner = "";
+    let message = "";
+    let result = "";
+    //there is a tie
+    if(player === computer){
+        
+    } 
+    if(
+        (player=='rock' && computer == 'scissors') ||
+        (player == 'paper' && computer=='rock') ||
+        (player == 'scissors' && computer == 'paper')
+    ) {
+        currentPlayerScore+=1; 
+        winner = "Player";
+        message = player + " beats " + computer;
+        console.log(message);
 
-console.log(game())
+        result = (winner + " wins! " + message 
+        + "<br><br>Player Score: " + currentPlayerScore + "<br>Computer Score: " + currentComputerScore);
+
+        if(currentPlayerScore == 5){
+            result+= '<br><br>'+winner+" won the game! Reload page to try again.";
+            disable();
+        }
+    } else if (player==computer){
+        message = "Tie";
+        console.log(message);
+        result = (message
+            +"<br><br>Player Score: " + currentPlayerScore + "<br>Computer Score: " + currentComputerScore);
+    } else {
+        currentComputerScore+=1;
+        winner = "Computer";
+        message = computer + " beats " + player; 
+        console.log(message);
+        result = (winner + " wins! " + message 
+        + "<br><br>Player Score: " + currentPlayerScore + "<br>Computer Score: " + currentComputerScore);
+        if(currentComputerScore == 5){
+            result+= '<br><br>'+winner+" won the game! Reload page to try again.";
+            disable();
+        }
+
+    }
+        document.getElementById('result').innerHTML = result;
+        return;
+}
+choices.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        playRound(button.value)
+    });
+});
